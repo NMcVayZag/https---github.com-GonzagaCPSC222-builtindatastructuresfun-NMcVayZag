cd4821e252d06eaf3fc31b1dde95775f68f3c68a
NMcVayZag <Nickwmcvay15@gmail.com>
2022-01-31 21:39:22 -0800

    initial

---
+++ Data_Structure_Fun.py
@@ -0,0 +1,8 @@
+#2D lists (grid-like)
+#3D lists (cube-like)
+#there are two ways to look at 2d lists
+#1st as a table 
+matrix = [  [1,2,3],
+            [4,5,6],
+            [7,8,9]]
+print(matrix[1][2])
